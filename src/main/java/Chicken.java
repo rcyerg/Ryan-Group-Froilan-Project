0d75153b6f752e4641222487397dab0ae9bda5f7
rcyerg <rcyerger02@gmail.com>
2024-04-19 13:03:35 -0400

    Update Chicken.java

--- src/main/java/Chicken.java
+++ src/main/java/Chicken.java
@@ -1,22 +1,19 @@
 public class Chicken extends Animal implements Produce{
+   
     private boolean hasBeenForFertilized;
-
-    public Chicken(boolean hasBeenForFertilized){
-        this.hasBeenForFertilized = hasBeenForFertilized;
-    }
 
     @Override
     public void eat() {
-        super.eat();
+
     }
 
     @Override
     public void makeNoise() {
-        super.makeNoise();
+
     }
 
     @Override
     public Edible yield() {
-        return null;
+   
     }
 }
