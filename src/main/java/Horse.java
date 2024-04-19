f69f8ba8afe3d67ec0fe6a2f4a9944c986197dfb
rcyerg <rcyerger02@gmail.com>
2024-04-19 13:03:02 -0400

    Update Horse.java

--- src/main/java/Horse.java
+++ src/main/java/Horse.java
@@ -1,11 +1,11 @@
 public class Horse extends Animal implements Eater, NoiseMaker, Rideable{
     @Override
     public void eat() {
-        super.eat();
+        
     }
 
     @Override
     public void makeNoise() {
-        super.makeNoise();
+        
     }
 }
